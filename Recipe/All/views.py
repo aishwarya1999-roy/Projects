1ee222360bfc70bdf713f1e07498c6627d25961e
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-30 12:22:05 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes.html')
+    return render(request, 'home.html')
 
 def base(request):
     return render(request, 'base.html')
