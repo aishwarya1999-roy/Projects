262eb976ac8ca98b5dc922cb0df655ea3dbec940
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 01:58:10 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
+    
+
+
+    return render(request, 'home.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def recipe(request):
     if request.method =="POST":
         data = request.POST
         
@@ -9,12 +18,4 @@ def home(request):
 
         print(recipe_description)
         print(recipe_name)
-
-        
-    return render(request, 'home.html')
-
-def base(request):
-    return render(request, 'base.html')
-
-def recipe(request):
     return render(request, 'recipe.html')
