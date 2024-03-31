838a43b812c921f15cf67b8f4107dcd0b3be10fe
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 11:16:12 +0530

    Auto-commit

--- Recipe/Recipe/urls.py
+++ Recipe/Recipe/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from All.views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name = "home"),
