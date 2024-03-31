2801850cc66015a3095be5022c26d18aa8820038
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 09:51:42 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -9,7 +9,7 @@ def base(request):
 def recipe(request):
     if request.method =="POST":
         data = request.POST
-        recipe_image = request.FILES['recipe_image']
+        recipe_image = request.FILES.get('recipe_image')
         recipe_name = data.get('recipe_name')
         recipe_description = data.get('recipe_description')
 
