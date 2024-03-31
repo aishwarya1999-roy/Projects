0a80567d1020370cf256846cdec73d2d5c42aad5
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 14:50:40 +0530

    Auto-commit

--- Recipe/All/form.py
+++ Recipe/All/form.py
@@ -3,5 +3,5 @@ from .models import  Recipe
 
 class  RecipeForm(forms.ModelForm):
     class Meta:
-        model =  Recipe
+        model = Recipe
         fields = ['field1', 'field2']  # Add other fields as needed
