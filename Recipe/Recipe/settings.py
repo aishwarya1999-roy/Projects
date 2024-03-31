5dfa41b9cbf616355b3761d708648de62cf407bd
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 11:13:45 +0530

    Auto-commit

--- Recipe/Recipe/settings.py
+++ Recipe/Recipe/settings.py
@@ -128,7 +128,8 @@ STATICFILES_DIR = {
     os.path.join(BASE_DIR, "public/static")
 }
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "public/static")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
