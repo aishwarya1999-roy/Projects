6854af62473505de14a8f3061a08c8fe5849e3fa
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 14:53:14 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -38,7 +38,7 @@ def update_recipe(request, id):
     queryset = Recipe.objects.get(id= id)
 
     if request.method =="POST":
-       form = YourModelForm(request.POST, instance=item)
+       form = RecipeForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
             return redirect('your_redirect_url')
