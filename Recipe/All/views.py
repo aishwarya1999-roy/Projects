49c0d540dd94df1326601a2fdb8ea9524e973188
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 15:56:58 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -28,10 +28,7 @@ def recipe(request):
 
     if request.GET.get('search'):
         queryset= queryset.filter(recipe_name__icontains = request.GET.get('search'))
-       
-    if request.GET.get('close-search'):
-        queryset= queryset.filter(recipe_name__icontains = request.GET.get('close-search'))
-
+        
     context = {'recipes' : queryset}
     return render(request, 'recipe.html', context)
 
