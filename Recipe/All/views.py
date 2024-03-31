32f2a7b9f78479b75f8467bb9925c36669610c4b
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 10:03:07 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -14,10 +14,10 @@ def recipe(request):
         recipe_name = data.get('recipe_name')
         recipe_description = data.get('recipe_description')
 
-    Recipe.objects.create(
-        recipe_image = recipe_image,
-        recipe_name = recipe_name,
-        recipe_description = recipe_description,
-    )
+        Recipe.objects.create(
+            recipe_image = recipe_image,
+            recipe_name = recipe_name,
+            recipe_description = recipe_description,
+        )
         
     return render(request, 'recipe.html')
