82d99fb725294efbf2826c9c676c2100e7a52819
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-03-31 15:04:00 +0530

    Auto-commit

--- Recipe/All/views.py
+++ Recipe/All/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
-from django.http import JsonResponse
-from .form import *
 
 def home(request):
     return render(request, 'home.html')
@@ -39,10 +37,17 @@ def update_recipe(request, id):
     queryset = Recipe.objects.get(id= id)
 
     if request.method =="POST":
-        form = RecipeForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save()
-            return redirect("/recipe/")
-    else:
-        form = RecipeForm(instance=item)
-    return render(request, 'update_item.html', {'form': form})
+        data = request.POST
+        recipe_image = request.FILES.get('recipe_image')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+
+        queryset.recipe_name = recipe_name
+        queryset.recipe_description = recipe_description
+        if recipe_image:
+            queryset.recipe_image = recipe_image
+
+        queryset.save()
+        return redirect("/recipe/")
+    context = {'recipe' : queryset}
+    return render(request, 'update_recipe.html', context)
