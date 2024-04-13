183e5ca853f81da267ff99d244657855652a5466
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-04-14 01:20:51 +0530

    Auto-commit

--- Food_Recommendation/Food_Recommendation/urls.py
+++ Food_Recommendation/Food_Recommendation/urls.py
@@ -16,9 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Home.views import *
+from All.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name = "home"),
+    path('base/', base, name = "base"),
+    path('recipe/', recipe, name = "recipe"),
+    path('search_page/', search_page, name = "search_page"),
+    path('delete_recipe/<id>/', delete_recipe, name = "delete_recipe"),
+    path('update_recipe/<id>/', update_recipe, name = "update_recipe"),
+    path('login_page/', login_page, name = "login_page"),
+    path('register_page/', register_page, name = "register_page"),
+    path('logout_page/', logout_page, name = "logout_page"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
