d2f88eb4ed82d2d60b023f2e1c7df56bab308607
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-04-14 01:25:21 +0530

    Auto-commit

--- Food_Recommendation/Food_Recommendation/settings.py
+++ Food_Recommendation/Food_Recommendation/settings.py
@@ -1,11 +1,6 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h_31z__vu6v4#813%j%zfal_3@f9r)ptucv6p9xp97as@&opq$'
